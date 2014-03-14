6945fef96cf2566e9048beefffd273b903574b53
LouisGV <lgvichy@gmail.com>
2014-03-14 02:16:50 -0700

    Update, Base for Angel

---
+++ Unity/Assets/Scripts/BehaviourScripts/AngelBehaviour.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AngelBehaviour : MonoBehaviour {
+
+	public Transform head, body, leftwing, rightwing;
+	// Use this for initialization
+	void Start () {
+			//Anything to initilize?...
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//if is moving -> Call the motion function(direction).
+	}
+
+	void Motion(Vector3 facingDirection){
+		//use force + gravity, then pull head and body back arcording to the facing.
+		//Might just move it first, then implement it with delta time later?
+	}
+}
